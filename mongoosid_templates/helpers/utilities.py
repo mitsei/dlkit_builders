2b39d98101e41fd9f16a15a8db13a97637dc253c
cjshaw <cjshaw@mit.edu>
2015-06-05 14:27:20 -0400

    base class for wrapper

--- mongoosid_templates/helpers/utilities.py
+++ mongoosid_templates/helpers/utilities.py
@@ -1,7 +1,43 @@
 """mongo utilities.py"""
 import json
-from .osid.osid_errors import NullArgument, NotFound
 
+from .osid.osid_errors import NullArgument, NotFound, OperationFailed
+
+from . import mongo_client
+
+
+class MongoClientValidated(object):
+    """automatically validates the insert_one, find_one, and delete_one methods"""
+    def __init__(self, db, collection):
+        self._mc = mongo_client[db][collection]
+
+    def _validate_write(self, result):
+        if (('writeErrors' in result and len(result['writeErrors']) > 0) or
+                ('writeConcernErrors' in result and len(result['writeConcernErrors']) > 0)):
+            raise OperationFailed(json.dumps(result))
+
+    def delete_one(self, query):
+        result = self._mc.find_one(query)
+        if result is None or result.deleted_count == 0:
+            raise NotFound(json.dumps(query) + ' returned None.')
+        return result
+
+    def find(self, query):
+        return self._mc.find(query)
+
+    def find_one(self, query):
+        result = self._mc.find_one(query)
+        if result is None:
+            raise NotFound(json.dumps(query) + ' returned None.')
+        return result
+
+    def insert_one(self, doc):
+        result = self._mc.insert_one(doc)
+        self._validate_write(result)
+
+    def save(self, doc):
+        result = self._mc.save(doc)
+        self._validate_write(result)
 
 def arguments_not_none(func):
     """decorator, to check if any arguments are None; raise exception if so"""
@@ -21,8 +57,3 @@ def arguments_not_none(func):
                 raise TypeError(ex.args[0])
 
     return wrapper
-
-def mongo_find(db, query):
-    results = db.find_one(**query)
-    if results is None:
-        raise NotFound('Did not find: ' + json.dumps(query))
