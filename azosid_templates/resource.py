d84773d16664c04310f096d52c77bf007b8ad6e0
Jeff Merriman <birdland@dhcp-18-111-5-237.dyn.mit.edu>
2015-09-30 14:28:17 -0400

    switched to kwargs in lookup style initers

--- azosid_templates/resource.py
+++ azosid_templates/resource.py
@@ -57,8 +57,8 @@ class ResourceManager:
             return getattr(sessions, '${return_type}')(
                 self._provider_manager.${method_name}(),
                 self._get_authz_session(),
-                self._get_hierarchy_session(),
-                query_session)
+                hierarchy_session=self._get_hierarchy_session(),
+                query_session=query_session)
         except AttributeError:
             raise OperationFailed()"""
 
@@ -74,8 +74,8 @@ class ResourceManager:
             return getattr(sessions, '${return_type}')(
                 self._provider_manager.${method_name}(${arg0_name}),
                 self._get_authz_session(),
-                self._get_hierarchy_session(),
-                query_session)
+                hierarchy_session=self._get_hierarchy_session(),
+                query_session=query_session)
         except AttributeError:
             raise OperationFailed()"""
 
@@ -127,8 +127,8 @@ class ResourceProxyManager:
                 self._provider_manager.${method_name}(proxy),
                 self._get_authz_session(),
                 proxy,
-                self._get_hierarchy_session(),
-                query_session)
+                hierarchy_session=self._get_hierarchy_session(),
+                query_session=query_session)
         except AttributeError:
             raise OperationFailed()"""
 
@@ -145,8 +145,8 @@ class ResourceProxyManager:
                 self._provider_manager.${method_name}(${arg0_name}, proxy),
                 self._get_authz_session(),
                 proxy,
-                self._get_hierarchy_session(),
-                query_session)
+                hierarchy_session=self._get_hierarchy_session(),
+                query_session=query_session)
         except AttributeError:
             raise OperationFailed()"""
 
@@ -176,10 +176,16 @@ class ResourceProxyManager:
 class ResourceLookupSession:
 
     init_template = """
-    def __init__(self, provider_session, authz_session, proxy=None, hierarchy_session=None, query_session=None):
+    def __init__(self, provider_session, authz_session, proxy=None, **kwargs):
         osid_sessions.OsidSession.__init__(self, provider_session, authz_session, proxy)
-        self._hierarchy_session = hierarchy_session
-        self._query_session = query_session
+        if 'hierarchy_session' in kwargs:
+            self._hierarchy_session = kwargs['hierarchy_session']
+        else:
+            self._hierarchy_session = None
+        if 'query_session' in kwargs:
+            self._query_session = kwargs['query_session']
+        else:
+            self._query_session = None
         self._qualifier_id = provider_session.get_${cat_name_under}_id()
         self._id_namespace = '${pkg_name}.${object_name}'
         self.use_federated_${cat_name_under}_view()
@@ -326,10 +332,12 @@ class ResourceLookupSession:
 class ResourceQuerySession:
 
     init_template = """
-    def __init__(self, provider_session, authz_session, proxy=None, hierarchy_session=None, query_session=None):
+    def __init__(self, provider_session, authz_session, proxy=None, **kwargs):
         osid_sessions.OsidSession.__init__(self, provider_session, authz_session, proxy)
-        self._hierarchy_session = hierarchy_session
-        #self._query_session = query_session
+        if 'hierarchy_session' in kwargs:
+            self._hierarchy_session = kwargs['hierarchy_session']
+        else:
+            self._hierarchy_session = None
         self._qualifier_id = provider_session.get_${cat_name_under}_id()
         self._id_namespace = '${pkg_name}.${object_name}'
         self.use_federated_${cat_name_under}_view()
