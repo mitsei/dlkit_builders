a0114b43c36ed175c1a6097bdf16f3bea0cea2fc
cjshaw <cjshaw@mit.edu>
2016-04-07 09:20:35 -0400

    with missing composition lookup session authz methods

--- azosid_templates/repository.py
+++ azosid_templates/repository.py
@@ -122,9 +122,29 @@ class CompositionLookupSession:
             self._query_session = kwargs['query_session']
         else:
             self._query_session = None
-
         self._qualifier_id = provider_session.get_${cat_name_under}_id()
         self._id_namespace = '${pkg_name_replaced}.${object_name}'
+        self.use_federated_${cat_name_under}_view()
+        self.use_comparative_${object_name_under}_view()
+
+    def _get_unauth_${cat_name_under}_ids(self, ${cat_name_under}_id):
+        if self._can('lookup', ${cat_name_under}_id):
+            return [] # Don't go further - assumes authorizations inherited
+        else:
+            unauth_list = [str(${cat_name_under}_id)]
+        if self._hierarchy_session.has_child_${cat_name_under_plural}(${cat_name_under}_id):
+            for child_${cat_name_under}_id in self._hierarchy_session.get_child_${cat_name_under}_ids(${cat_name_under}_id):
+                unauth_list = unauth_list + self._get_unauth_${cat_name_under}_ids(child_${cat_name_under}_id)
+        return unauth_list
+
+    def _try_harder(self, query):
+        if self._hierarchy_session is None or self._query_session is None:
+            # Should probably try to return empty result instead
+            # perhaps through a query.match_any(match = None)?
+            raise PermissionDenied()
+        for ${cat_name_under}_id in self._get_unauth_${cat_name_under}_ids(self._qualifier_id):
+            query.match_${cat_name_under}_id(${cat_name_under}_id, match=False)
+        return self._query_session.get_${object_name_under_plural}_by_query(query)
 """
     # Note: These should be templated some day. But first add to pattern mappers
 
