5cffed285e9df6698af05180320578c61dcf9a54
cjshaw <cjshaw@mit.edu>
2016-07-18 08:37:04 -0400

    fix proxy in get_avatar

--- mongoosid_templates/resource.py
+++ mongoosid_templates/resource.py
@@ -1538,7 +1538,7 @@ ${instance_initers}"""
         mgr = self._get_provider_manager('${return_pkg_caps}')
         if not mgr.supports_${return_type_under}_lookup():
             raise errors.OperationFailed('${return_pkg_title} does not support ${return_type} lookup')
-        lookup_session = mgr.get_${return_type_under}_lookup_session() # What about the Proxy
+        lookup_session = mgr.get_${return_type_under}_lookup_session(proxy=getattr(self, "_proxy", None)
         lookup_session.use_federated_${return_cat_name_under}_view()
         osid_object = lookup_session.get_${return_type_under}(self.get_${var_name}_id())
         return osid_object"""
