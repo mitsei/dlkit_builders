072fa054a29398565d8667a43c403ef53bbd9e0a
cjshaw <cjshaw@mit.edu>
2016-08-04 11:04:47 -0400

    fix spacing

--- mongoosid_templates/assessment.py
+++ mongoosid_templates/assessment.py
@@ -1849,13 +1849,13 @@ class AssessmentSection:
         question = item.get_question()
         try:
             matching_questions = [q['displayElements']
-                                 for q in self._my_map['questions']
-                                 if q['questionId'] == str(question_id)]
+                                  for q in self._my_map['questions']
+                                  if q['questionId'] == str(question_id)]
             if len(matching_questions) > 0:
                 new_display_name = matching_questions[0]
                 new_display_name = [str(e) for e in new_display_name]
                 question.set_display_label('.'.join(new_display_name))
-                
+
         except AttributeError:
             pass
         return question
