35e2bd7c50482684185154207967c5b1d79f06a3
cjshaw <cjshaw@mit.edu>
2017-03-15 09:53:54 -0400

    add BasicCoordinate to services primitives

--- kitosid_templates/osid_helpers/primitives.py
+++ kitosid_templates/osid_helpers/primitives.py
@@ -9,3 +9,4 @@ from dlkit.primordium.transport.objects import DataInputStream
 from dlkit.primordium.calendaring.primitives import DateTime, Duration
 from dlkit.primordium.mapping.color_primitives import RGBColorCoordinate
 from dlkit.primordium.mapping.spatial_units import RectangularSpatialUnit
+from dlkit.primordium.mapping.coordinate_primitives import BasicCoordinate
