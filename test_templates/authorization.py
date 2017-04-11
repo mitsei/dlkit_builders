2c1ff1c4046d423120b7316e4c7d5ac5e1de08f3
cjshaw <cjshaw@mit.edu>
2017-04-11 14:35:35 -0400

    update impl to use JSON_1

--- test_templates/authorization.py
+++ test_templates/authorization.py
@@ -28,7 +28,7 @@ class AuthorizationSession:
     init = """
     @classmethod
     def setUpClass(cls):
-        cls.authz_mgr = Runtime().get_manager('AUTHORIZATION', implementation='MONGO_1')
+        cls.authz_mgr = Runtime().get_manager('AUTHORIZATION', implementation='JSON_1')
         cls.vault_admin_session = cls.authz_mgr.get_vault_admin_session()
         cls.vault_lookup_session = cls.authz_mgr.get_vault_lookup_session()
 
