c0d478ceab13e04e6d096adbde38e1f5c75b6993
birdland <birdland@dhcp-18-189-91-224.dyn.MIT.EDU>
2017-07-06 13:37:54 -0400

    overriding cataloging CatalogSession and CatalogAssignmentSession methods

---
+++ jsonosid_templates/cataloging.py
@@ -0,0 +1,26 @@
+
+class CatalogSession:
+
+    get_ids_by_catalog = """
+        raise errors.Unimplemented"""
+
+    get_ids_by_catalogs = """
+        raise errors.Unimplemented"""
+
+    get_catalog_ids_by_id = """
+        raise errors.Unimplemented"""
+
+    get_catalogs_by_id = """
+        raise errors.Unimplemented"""
+
+
+class CatalogAssignmentSession:
+
+    assign_id_to_catalog = """
+        raise errors.Unimplemented"""
+
+    unassign_id_from_catalog = """
+        raise errors.Unimplemented"""
+
+    reassign_id_to_catalog = """
+        raise errors.Unimplemented"""
